cdf52934c398fe780b066177990ef4e322c639b6
Bridget Goodwine <bgoodwin@nd.edu>
2022-10-24 15:27:56 -0400

    show tables

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # NDEats-Back-End
 
 [tutorial](https://realpython.com/python-mysql/#installing-mysql-server-and-mysql-connectorpython)
+
+Users table
+```
+CREATE TABLE users(
+    id INT PRIMARY KEY, # userinfo_response.json()["sub"]
+    name VARCHAR(100),  # userinfo_response.json()["given_name"]
+    email VARCHAR(100), # userinfo_response.json()["email"]
+)
+``` 
--- sql.py
+++ sql.py
@@ -1,14 +1,45 @@
 #!/usr/bin/env python3
+
 from getpass import getpass
 from mysql.connector import connect, Error
 
-try:
-    with connect(
-        host="localhost",
-        user=input("Enter username: "),
-        password=getpass("Enter password: "),
-        database="cse30246",
-    ) as connection:
-        print(connection)
-except Error as e:
-    print(e)
+# info from google sign in
+# unique_id = userinfo_response.json()["sub"]
+# users_email = userinfo_response.json()["email"]
+# users_name = userinfo_response.json()["given_name"]
+create_users_table_query = """
+CREATE TABLE users(
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100)
+)
+"""
+
+def show_tables(connection):
+    users_query = "DESCRIBE users"
+    users_query = "DESCRIBE users"
+    
+    with connection.cursor() as cursor:
+        cursor.execute(users_query)
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
+        
+def main():
+    
+    # try to connect to SQL server and print current tables
+    try:
+        with connect(
+            host="localhost",
+            user=input("Enter username: "),
+            password=getpass("Enter password: "),
+            database="bgoodwin",
+        ) as connection:
+            print(connection)
+            show_tables(connection)
+
+    except Error as e:
+        print(e)
+
+if __name__ == "__main__":
+    main()
