d453861916ba3248b951f811b31e1560748bfef9
Jean Francois Boueri Alvarez <jboueri@nd.edu>
2022-10-26 15:29:05 -0400

    #1 - Get order queries

--- user_queries/user_functions.py
+++ user_queries/user_functions.py
@@ -5,6 +5,8 @@ from mysql.connector import connect, Error
 
 # Globals
 USER_TABLE = 'Users' 
+ORDER_TABLE = 'Orders'
+OLD_ORDER_TABLE = 'OldOrders'
 
 # Functions
 def add_user_to_Users(connection, username, name, email):
@@ -15,15 +17,67 @@ def add_user_to_Users(connection, username, name, email):
         VALUES ({username}, '{name}', '{email}')
     '''
 
-    # Check if user already in Users, but check is done internally so we're good
-
     # Execute Query using Cursor
     cursor = connection.cursor()
-    cursor.execute(mysql_user_insert_query)
+
+    # Check if user already in Users
+    try:
+        cursor.execute(mysql_user_insert_query)
+    except Error as e:
+        print(f'User already exists in database')
+    
     connection.commit()
 
     # Close cursor
     cursor.close()
+
+def get_user_orders_from_Orders(connection, username):
+
+    # Define the Query
+    mysql_get_user_orders_query = f'''
+        SELECT *
+        FROM {ORDER_TABLE}
+        WHERE orderer_id_fk = '{username}' OR deliverer_id_fk = '{username}'
+    '''
+
+    # Execute Query using Cursor
+    cursor = connection.cursor()
+    cursor.execute(mysql_get_user_orders_query)
+
+    # Print out result
+    result = cursor.fetchall()
+    print("Results from Orders:")
+    for row in result:
+        print(row)
+
+    # Close Cursor
+    cursor.close()
+
+
+def get_user_orders_from_OldOrders(connection, username):
+
+    # Define the Query
+    mysql_get_user_old_orders_query = f'''
+        SELECT *
+        FROM {OLD_ORDER_TABLE}
+        WHERE orderer_id_fk = '{username}' OR deliverer_id_fk = '{username}'
+    '''
+
+    # Execute Query using Cursor
+    cursor = connection.cursor()
+    cursor.execute(mysql_get_user_old_orders_query)
+
+    # Print out result
+    result = cursor.fetchall()
+    print("Results from Old Orders:")
+    for row in result:
+        print(row)
+
+    # Close Cursor
+    cursor.close()
+
+
+
 
 # Main Execution
 def main():
@@ -32,7 +86,10 @@ def main():
             # Do Stuff
 
             # TODO: Get these values from Google Sign in!! (we wanna execute this anytime someone signs in)
-            add_user_to_Users(connection, 29492, 'Jean Francois Boueri', 'jbueri@nd.edu')
+            add_user_to_Users(connection, 293942, 'Jean Francois Boueri', 'jbueri@nd.edu')
+
+            get_user_orders_from_Orders(connection, 29492)
+            get_user_orders_from_OldOrders(connection, 29492)
 
     except Error as e:
         print(e)
