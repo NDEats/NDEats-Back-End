378ef0667db364949a79c3c54ed1b3880d4bbfa0
Bridget Goodwine <bgoodwin@nd.edu>
2022-10-29 11:58:06 -0400

    #13 - switch from default sqllite to our mysql database

--- ndeats/ndeats/settings.py
+++ ndeats/ndeats/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ndeats.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE'  : 'django.db.backends.mysql',
+        'NAME'    : BASE_DIR / 'jboueri',
+        'USER'    : 'jboueri',
+        'PASSWORD': 'pwpwpwpw',
+        'HOST'    : 'localhost',
+        'PORT'    : '3306',
     }
 }
 
