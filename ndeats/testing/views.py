f1d13fb3a6c9671530f73cb070962e3f4040db7e
Bridget Goodwine <bgoodwin@nd.edu>
2022-10-29 13:49:53 -0400

    #13 - made post to add order

--- ndeats/testing/views.py
+++ ndeats/testing/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+import json
+from .models import Order
 
-# Create your views here.
+class Order(View):
+    def post(self, request):
+        # get user specified data
+        data = json.loads(request.body.decode("utf-8"))
+        do = data.get('dropoff')
+        pu = data.get('pickup')
+        t = data.get('tip')
+        oid = data.get('ordererId')
+        rb = data.get('readyBy')
+
+        # set default values user cannot specify
+        did = None # delivererId
+        a = True   # available
+        
+        # save order data in dict to create order class
+        orderData = {
+            'dropoff' : do,
+            'pickup' : pu,
+            'tip' : t,
+            'delivererId': did,
+            'ordererId': oid,
+            'available': a,
+            'readyBy': rb,
+        }
+        
+        # create instance of order (automatically stores order in orders table)
+        order = Order.objects.create(**orderData)
+
+        # send response with order ID
+        data = {
+            'message': f'Order created with ID: {order.id}'
+        }
+        return JsonResponse(data, status=201)
+
