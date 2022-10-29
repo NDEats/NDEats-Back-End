3ccffb71f073e2242f9f415c78483ba55fcb5611
Jean Francois Boueri Alvarez <jboueri@nd.edu>
2022-10-29 12:21:21 -0400

    #13 - Trying to make a simple model

--- ndeats/testing/models.py
+++ ndeats/testing/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 class Users(models.Model):
+    user_id = models.IntegerField()
+    user_name = models.CharField(max_length = 100)
+    user_email = models.CharField(max_length = 100)
