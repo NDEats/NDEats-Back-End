3c6d9a26aa7b79b30b7bf969e17e47d89f79663f
Bridget Goodwine <bgoodwin@nd.edu>
2022-10-31 18:05:17 -0400

    #18 - error checking for pickup

--- ndeats/testing/views.py
+++ ndeats/testing/views.py
@@ -72,6 +72,12 @@ class OrderUpdate(View):
          
         data = json.loads(request.body.decode("utf-8"))
         order = OrderModel.objects.get(id=order_id)
+        if not order.available:
+            data = {
+                'message': f'Order {order_id} is not available for pickup'
+            }
+            return JsonResponse(data)
+
         order.available = False # False = unavailable
         order.delivererId = PersonModel.objects.get(id=data['deliverer'])
         order.save()
