0755aa69a4ef4f560e4d4701e2d5d75c9f7ed913
Bridget Goodwine <bgoodwin@nd.edu>
2022-12-03 15:50:07 -0500

    #31 - include deliverer in old order return

--- ndeats/testing/views.py
+++ ndeats/testing/views.py
@@ -128,6 +128,7 @@ class Person(View):
                 'pickup' : order.pickup,
                 'tip' : order.tip,
                 'ordererId' : model_to_dict(order.ordererId),
+                'delivererId' : model_to_dict(order.delivererId),
                 'readyby' : order.readyBy
             })
         
