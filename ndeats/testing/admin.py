f204513b79ee0b5ee89e98c7a2671654f54b2c17
Christopher Boumalhab <cboumalh@nd.edu>
2022-12-25 16:49:37 -0500

    removed unecessary folders

--- ndeats/testing/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Person, Order, OldOrder
-
-# Register your models here.
-admin.site.register(Person)
-admin.site.register(Order)
-admin.site.register(OldOrder)
